5962e166f2c0bc819209fe30e9823e767e78f85e
rina-25 <irina.kim.25@seoulforeign.org>
2021-04-29 18:52:52 +0900

    moving blob2 and blob3

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,11 @@ const blob2 = document.querySelector('#blob2')
 const blob3 = document.querySelector('#blob3')
 
 window.setInterval(() => {
-    const cy = parseInt(blob1.getAttribute('cy'))
-    blob1.setAttribute('cy', cy - 1)
+    const cy1 = parseInt(blob1.getAttribute('cy'))
+    blob1.setAttribute('cy', cy1 - 1)
+    const cy2 = parseInt(blob3.getAttribute('cy'))
+    blob3.setAttribute('cy', cy2 + 1)
+    const cy3 = parseInt(blob2.getAttribute('cy'))
+    blob2.setAttribute('cy', cy3 + 1)
 }, 100)
+
